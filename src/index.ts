98abcea5aee856ff8998f3c4bb21904db693c251
Somsubhra Das <das.somsubhra1@gmail.com>
2022-02-14 16:27:22 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import "reflect-metadata";
+import express from "express";
+
+async function bootstrap() {
+  // Build schema
+  // Init express
+  // create the apollo server
+  // start server
+  // apply middlewares
+  // listen app
+  // connect to db
+}
+
+bootstrap();
